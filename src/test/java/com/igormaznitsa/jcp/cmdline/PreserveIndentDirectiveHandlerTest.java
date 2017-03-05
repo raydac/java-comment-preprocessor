515b2bb8edca39b1b2375b5f940e611e8684c375
Igor Maznitsa <rrg4400@gmail.com>
2017-03-05 20:29:07 +0200

    added test for preserveindent cli directive

---
+++ src/test/java/com/igormaznitsa/jcp/cmdline/PreserveIndentDirectiveHandlerTest.java
@@ -0,0 +1,57 @@
+/* 
+ * Copyright 2014 Igor Maznitsa (http://www.igormaznitsa.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.igormaznitsa.jcp.cmdline;
+
+import com.igormaznitsa.jcp.context.PreprocessorContext;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class PreserveIndentDirectiveHandlerTest extends AbstractCommandLineHandlerTest {
+
+  private static final PreserveIndentDirectiveHandler HANDLER = new PreserveIndentDirectiveHandler();
+
+  @Override
+  public void testThatTheHandlerInTheHandlerList() {
+    assertHandlerInTheHandlerList(HANDLER);
+  }
+
+  @Override
+  public void testExecution() throws Exception {
+    final PreprocessorContext mock = preparePreprocessorContext();
+
+    assertFalse(HANDLER.processCommandLineKey("/Pi:", mock));
+    assertFalse(HANDLER.processCommandLineKey("/PI ", mock));
+    verify(mock, never()).setPreserveIndent(anyBoolean());
+
+    assertTrue(HANDLER.processCommandLineKey("/PI", mock));
+    verify(mock).setPreserveIndent(true);
+    reset(mock);
+
+    assertTrue(HANDLER.processCommandLineKey("/PI", mock));
+    verify(mock).setPreserveIndent(true);
+    reset(mock);
+  }
+
+  @Override
+  public void testName() {
+    assertEquals("/PI", HANDLER.getKeyName());
+  }
+
+  @Override
+  public void testDescription() {
+    assertDescription(HANDLER);
+  }
+}
