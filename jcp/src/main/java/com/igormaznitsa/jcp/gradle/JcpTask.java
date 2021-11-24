3975572316a725e4889c2bee8baeae68efcbd81a
Igor Maznitsa <rrg4400@gmail.com>
2021-11-24 23:20:15 +0200

    fixed compatibility with gradle 7, added tests

--- jcp/src/main/java/com/igormaznitsa/jcp/gradle/JcpTask.java
+++ jcp/src/main/java/com/igormaznitsa/jcp/gradle/JcpTask.java
@@ -27,11 +27,7 @@ import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.OutputFiles;
-import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.TaskExecutionException;
+import org.gradle.api.tasks.*;
 import org.gradle.execution.commandline.TaskConfigurationException;
 
 public class JcpTask extends DefaultTask {
@@ -196,10 +192,12 @@ public class JcpTask extends DefaultTask {
     return this.outcomingFiles;
   }
 
+  @Internal
   public FileCollection getIncomingFiles() {
     return this.incomingFiles;
   }
 
+  @Internal
   @Override
   public String getDescription() {
     return "Preprocess sources and resources with JCP";
