4e1809dfcfec98785b71502521ecae0fc08fcb59
Igor Maznitsa <rrg4400@gmail.com>
2024-06-07 01:20:50 +0300

    minor refactoring

--- jcp-tests/jcp-test-maven/src/test/java/com/igormaznitsa/jcp/it/maven/TestMain.java
+++ jcp-tests/jcp-test-maven/src/test/java/com/igormaznitsa/jcp/it/maven/TestMain.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestMain {
+class TestMain {
 
   @Test
-  public void testMain(){
+  void testMain(){
       //$MainTwo obj = new MainTwo();
       //#-
       Main obj = new Main();
