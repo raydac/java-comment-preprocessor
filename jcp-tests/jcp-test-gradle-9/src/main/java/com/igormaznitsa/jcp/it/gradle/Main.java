6d7da837c5d7e0bba870ce7a6c5c75767a419dd9
Igor Maznitsa <rrg4400@gmail.com>
2025-07-12 22:38:23 +0300

    added test for gradle 9

---
+++ jcp-tests/jcp-test-gradle-9/src/main/java/com/igormaznitsa/jcp/it/gradle/Main.java
@@ -0,0 +1,33 @@
+// #outname "MainTwo.java"
+package com.igormaznitsa.jcp.it.gradle;
+
+//$public class MainTwo {
+//#//
+public class Main {
+//#action "hello","world",$user2(1,2)
+  //$$public MainTwo(){
+  //#//
+  public Main(){
+    if (!this.getClass().getName().endsWith("MainTwo")) {
+      throw new Error("Must be MainTwo but detected"+this.getClass().getName());
+    }
+
+    final String test = /*$"\""+some.test.global+someGlobalVar+"\";"$*/ /*-*/ "";
+
+    if ("Some Test Global ValueHUZZAA!".equals(test)){
+      System.out.println("All ok, detected value '/*$some.test.global+someGlobalVar$*/'");
+    } else {
+      throw new Error("Unexpected value: "+test);
+    }
+  }
+
+  public String getValue(){
+    return /*$"\""+some.test.global+someGlobalVar+"\";"$*/ /*-*/ "";
+  }
+  
+  public static void main(String [] args) {
+    //$$new MainTwo();
+    //#//
+    new Main();
+  }
+}
